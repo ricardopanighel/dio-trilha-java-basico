e3a8595e05d1adcdbb32c279ac296df50f893b51
ricardopanighel <ricardopanighellazarini@gmail.com>
2024-09-02 18:56:45 -0300

    Finalizada a aula de Tipos e Variáveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,4 +1,13 @@
 package src;
 
 public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+    }
 }
